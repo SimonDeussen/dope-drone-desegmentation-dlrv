679798a154c81b6525e5f4a590bbfd07fe4c1e23
malwaru <malikawarunamal@gmail.com>
2022-01-12 18:26:54 +0100

    Correct erros in file import errors and IoU formula

--- code/evaluate.py
+++ code/evaluate.py
@@ -7,7 +7,6 @@ import os
 from imutils import paths
 from UNetSegmentation import utilities
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
 
 
@@ -21,25 +20,19 @@ def evalution_metics(groundTruth,prediction):
 
 	#Adding a smoothing operation to avoid division 0 error
     EPS=1e-6
-    origMask=groundTruth.copy()
-    print(f"Orig shape {origMask.shape}")
-    print(origMask)
-    origMask=np.where(origMask==255,1,origMask)
+    origMask=groundTruth.copy()  
     predMask=prediction.copy()
-    predMask=np.where(predMask==255,1,predMask)
     intersection=(predMask&origMask).sum((0,1))    
     union=(predMask | origMask).sum((0,1))
-    iou=((intersection+EPS)/(union+EPS))*100
-    
+    iou=((intersection+EPS)/(union+EPS))*100    
     dice=2.*(predMask*origMask).sum() / (predMask+origMask).sum()
-    # print(f" The IOU is {iou}")
-    # print(f"The F1 score is {dice}")
+    
     
     return iou,dice
 
 
 def prepare_plot(origImage, origMask, predMask,metric):
-# initialize our figure
+    # initialize our figure
     figure, ax = plt.subplots(nrows=1, ncols=3, figsize=(10, 10))
     # plot the original image, its mask, and the predicted mask
     ax[0].imshow(origImage)
@@ -56,7 +49,7 @@ def prepare_plot(origImage, origMask, predMask,metric):
     plt.show()
 
 
-if __name__=="__main__":
+def main():
 
     CURRENT_PATH=os.getcwd()
     DATASET_PATH=os.path.join(CURRENT_PATH,"data/zain_results")
@@ -70,15 +63,32 @@ if __name__=="__main__":
     groundtruthPaths=sorted(list(paths.list_images(GROUNDTRUTH_PATH)))
     maskPaths = sorted(list(paths.list_images(PREDICTIONS_PATH)))
 
+    total_IoU=0
 
-    for index in range(len(imagePaths)):
-        oriImage=mpimg.imread(imagePaths[index])
-        groundTruthImage=mpimg.imread(groundtruthPaths[index])
-        maskImage=mpimg.imread(maskPaths[index])
+    for index in range(0,len(imagePaths)):
+        oriImage=cv2.imread(imagePaths[index])
+        groundTruthImage=cv2.imread(groundtruthPaths[index],0)
+        maskImage=cv2.imread(maskPaths[index],0)
         IoU,dice=evalution_metics(groundTruthImage,maskImage)
-        plot_title="IoU = "+np.array2string(np.round(IoU),2)+"% Dice Score= "+np.array2string(np.round(dice,2))
-        prepare_plot(oriImage,groundTruthImage,maskImage,plot_title)
-       
-        if index==3:
-            break
+        total_IoU+=IoU       
 
+        if index<5:
+            #Prnints only the first 4 plots
+
+            plot_title="IoU = "+np.array2string(np.round(IoU),2)+"% Dice Score= "+np.array2string(np.round(dice,2))
+            prepare_plot(oriImage,groundTruthImage,maskImage,plot_title)
+
+
+    average_iou=total_IoU/len(imagePaths)
+    print(f"Average IoU is {average_iou}")
+
+
+
+
+
+
+
+if __name__=="__main__":
+
+    main()
+    
