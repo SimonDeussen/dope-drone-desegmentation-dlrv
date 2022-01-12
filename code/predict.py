50afa3fad8a4e4e34fda2f78345857dd41bf189c
malwaru <malikawarunamal@gmail.com>
2022-01-12 20:02:16 +0100

    Removed unncessary functions from predict.py

--- code/predict.py
+++ code/predict.py
@@ -6,48 +6,8 @@ import numpy as np
 import torch
 import cv2
 import os
-from evaluate import evalution_metics
+from evaluate import evalution_metics,prepare_plot
 
-
-def evaluate(groundTruth,prediction):
-
-	#Adding a smoothing operation to avoid division 0 error
-	EPS=1e-6
-	origMask=groundTruth.copy()
-	predMask=prediction.copy()
-
-
-	intersection=(predMask&origMask).sum((0,1))
-	union=(predMask | origMask).sum((0,1))
-	iou=((intersection+EPS)/(union+EPS))*100
-	dice= 2.*(predMask*origMask).sum() / (predMask+origMask).sum()
-
-	print(f" The IOU is {round(iou,2)}%")
-	print(f"The F1 score is {dice} \n")
-	# print(f"Intersections is {union} total pixles {total_pixels}")
-
-	return iou
-
-
-
-def prepare_plot(origImage, origMask, predMask,metric):
-	# initialize our figure
-	figure, ax = plt.subplots(nrows=1, ncols=3, figsize=(10, 10))
-	# plot the original image, its mask, and the predicted mask
-	ax[0].imshow(origImage)
-	ax[1].imshow(origMask)
-	ax[2].imshow(predMask)
-	# set the titles of the subplots
-	ax[0].set_title("Image")
-	ax[1].set_title("Original Mask")
-	ax[2].set_title("Predicted Mask")
-	
-
-	# set the layout of the figure and display it
-	figure.tight_layout()
-	figure.suptitle(metric)
-	# figure.show()
-	plt.show()
 
 def make_predictions(model,imagePath,count):
 	# set model to evaluation mode
